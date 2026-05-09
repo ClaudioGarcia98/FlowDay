bbf653a6607bf89b77c17d244471629ad2ab3d4a
CLAUDIO\claud <claudiogarcia141998@gmail.com>
2026-05-09 20:50:38 +0100

    small change to test if PR works

--- app/src/main/java/dev/flowday/app/MainActivity.kt
+++ app/src/main/java/dev/flowday/app/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             FlowDayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Claudio",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     FlowDayTheme {
-        Greeting("Android")
+        Greeting("Claudio")
     }
 }
